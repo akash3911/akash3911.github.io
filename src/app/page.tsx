34b6eb63b4f3167ff41c12edead00a2cb25cb44a
JACCKKK7 <akash.22bce20488@vitapstudent.ac.in>
2025-07-25 19:03:54 +0530

    aaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -487,7 +487,7 @@ export default function Page() {
                 <div className="flex justify-center mt-8">
                   <button
                     onClick={handleShowMoreProjects}
-                    className="text-muted-foreground hover:text-background hover:bg-foreground inline-flex items-center space-x-2 group transition-all duration-300"
+                    className="text-muted-foreground hover:text-foreground inline-flex items-center space-x-2 group transition-all duration-300"
                   >
                     <div className="flex items-center space-x-2 group-hover:translate-y-[-2px] transition-all duration-200">
                       <span>Show More</span>
@@ -499,7 +499,7 @@ export default function Page() {
                 <div className="flex justify-center mt-8">
                   <button
                     onClick={handleShowMoreProjects}
-                    className="text-muted-foreground hover:text-background hover:bg-foreground inline-flex items-center space-x-2 group transition-all duration-300"
+                    className="text-muted-foreground hover:text-foreground inline-flex items-center space-x-2 group transition-all duration-300"
                   >
                     <div className="flex items-center space-x-2 group-hover:translate-y-[-2px] transition-all duration-200">
                       <span>Show Less</span>
@@ -833,7 +833,7 @@ export default function Page() {
                 <div className="flex justify-center">
                   <button
                     onClick={handleShowMoreCerts}
-                    className="text-muted-foreground hover:text-foreground inline-flex items-center space-x-2 transition-colors group hover:invert hover:brightness-0 hover:contrast-100 transition-all duration-300"
+                    className="text-muted-foreground hover:text-foreground inline-flex items-center space-x-2 group transition-all duration-300"
                   >
                     <div className="flex items-center space-x-2 group-hover:translate-y-[-2px] transition-all duration-200">
                       <span>Show More</span>
@@ -848,7 +848,7 @@ export default function Page() {
                 <div className="flex justify-center">
                   <button
                     onClick={handleShowMoreCerts}
-                    className="text-muted-foreground hover:text-background hover:bg-foreground inline-flex items-center space-x-2 group transition-all duration-300"
+                    className="text-muted-foreground hover:text-foreground inline-flex items-center space-x-2 group transition-all duration-300"
                   >
                     <div className="flex items-center space-x-2 group-hover:translate-y-[-2px] transition-transform duration-200">
                       <span>Show Less</span>
