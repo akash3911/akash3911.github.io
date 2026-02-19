ca57cddea0e2c93540e96692215d5d4ae15939fd
Akash Jonnalagadda <akash.22bce20488@vitapstudent.ac.in>
2026-02-19 18:16:50 +0530

    Refactor next.config.mjs by removing unused variables
    Removed production environment checks and base path configurations.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'my-portfolio';
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
     formats: ['image/webp'],
